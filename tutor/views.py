6f4a43e7bb64ee2170019c55b6545ec7076a296b
Nerzhul <sinokbati@gmail.com>
2023-04-08 15:12:06 +0300

    date time view

--- tutor/views.py
+++ tutor/views.py
@@ -10,8 +10,8 @@ def lessons(request):
         a[student.first_name] = []
         for lesson in student.lesson.values_list('start_datetime', 'end_datetime'):
             datetime_list = [
-                lesson.start_datetime.strftime("%Y %m %d %H:%M"),
-                lesson.end_datetime.strftime("%Y %m %d %H:%M")
+                lesson[0].strftime("%Y %m %d %H:%M"),
+                lesson[1].strftime("%Y %m %d %H:%M")
             ]
             a[student.first_name].append(datetime_list)
     return JsonResponse(a)
