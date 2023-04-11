567f65b679a4bdc8618452e8b00a12a1abf4c5d6
VLADoookkk <grigorev.vld76@gmail.com>
2023-04-11 14:54:30 +0300

    removed the lesson in urls.py

--- vlad_project/urls.py
+++ vlad_project/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from tutor.views import lessons, create, index
+from tutor.views import create, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('lessons', lessons),
     path('create/', create),
 ]
